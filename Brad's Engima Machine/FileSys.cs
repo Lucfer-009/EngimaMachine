a29bd33b8dfca5418e69b1af8d46a6c68b991289
Brad <bradspam.bw@gmail.com>
2021-11-10 18:54:47 +0000

    Creation of intial class structures

---
+++ Brad's Engima Machine/FileSys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brad_s_Engima_Machine
+{
+    class FileSys
+    {
+    }
+}
