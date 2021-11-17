e803090c65379dace6ba7f135029769918908516
Bradley Whitefield <018275@cirencester.ac.uk>
2021-11-17 11:57:41 +0000

    Creation of Cog and Lookup functions

--- Brad's Engima Machine/Cog.cs
+++ Brad's Engima Machine/Cog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,67 @@ namespace Brad_s_Engima_Machine
 {
     class Cog
     {
+        private int size;
+        private string cogFileLocation = @"F:\VS Projects\Project\Lucfer-009\EngimaMachine\Brad's Engima Machine\cogs\";
+        private CogArray cypher;
+
+        public Cog(int size, int cogChosen, int currentPos, int ringPos)
+        {
+            cogFileLocation += $"{cogChosen}.txt";
+            int startShift = (currentPos - ringPos) % size;
+            this.size = size;
+            cypher = new CogArray(this, startShift);
+        }
+
+
+        public int GetSize() { return size; }
+        public int GetTurnover() { return turnover; }
+        public string GetCogFileLocation() { return cogFileLocation; }
 
     }
 
     class CogArray : LookupArray
     {
+        private int shift;
+        private int turnover;
+        public CogArray(Cog click, int shift) : base(click.GetSize(), click.GetCogFileLocation())
+        {
+            this.shift = shift;
+            shiftArray = FileToShiftArray();
+        }
 
+        protected override int forwardParse(int letterIndex)
+        {
+            int x = (shiftArray[(letterIndex + shift) % size] + letterIndex) % size;
+            return x;
+        }
+
+        public int reverseParse(int letterIndex)
+        {
+            int x = (shiftArray[letterIndex - shift % size] + letterIndex) % size;
+            return x;
+        }
+
+        protected override int[] FileToShiftArray()
+        {
+            int[] current = new int[size];
+            string key = "";
+            StreamReader sr = new StreamReader(fileLocation);
+            key = sr.ReadLine();
+            sr.Close();
+
+            string[] slice = key.Split(','); // Cog file needs to read the turnover from a file
+            key = slice[1];
+            turnover = GU.AlphaCharToIntIndex(Convert.ToChar(slice[0]));
+
+            int count = 0;
+            foreach (char y in key)
+            {
+                current[count] = Convert.ToInt32(y) - 65 - count;
+                count++;
+            }
+
+            return current;
+        }
     }
 }
--- Brad's Engima Machine/LookupArray.cs
+++ Brad's Engima Machine/LookupArray.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,40 @@ namespace Brad_s_Engima_Machine
 {
     class LookupArray
     {
-        private int[] shiftArray;
+        protected int[] shiftArray;
+        protected int size;
+        protected string fileLocation;
 
-        public LookupArray(int size)
+        public LookupArray(int size, string fileLocation)
         {
             shiftArray = new int[size];
+            shiftArray = FileToShiftArray();
+            this.size = size;
+            this.fileLocation = fileLocation;
         }
 
+        protected virtual int forwardParse(int letterIndex)
+        {
+            int x = (shiftArray[(letterIndex % size)] + letterIndex) % size;
+            return x;
+        }
+
+        protected virtual int[] FileToShiftArray()
+        {
+            int[] current = new int[size];
+            string key = "";
+            StreamReader sr = new StreamReader(fileLocation);
+            key = sr.ReadLine();
+            sr.Close();
+
+            int count = 0;
+            foreach (char y in key)
+            {
+                current[count] = Convert.ToInt32(y) - 65 - count;
+                count++;
+            }
+
+            return current;
+        }
     }
 }
