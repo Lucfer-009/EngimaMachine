39ef7036ac5762ff1328f001e4613c7c4027b4a1
Bradley Whitefield <018275@cirencester.ac.uk>
2022-01-05 09:53:58 +0000

    Ammended a FileLocationHandler, for differences on machines

---
+++ Brad's Engima Machine/FileLocationHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brad_s_Engima_Machine
+{
+    static class FileLocationHandler
+    {
+        static private string mainDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        static private string settingsPath = @"\settings\";
+        static private string masterConfigPath = mainDirectory + settingsPath;
+
+        static public readonly string logFileLocation_R = masterConfigPath + @"log files\";
+        static public readonly string machineData_R = masterConfigPath + @"machine data\";
+        static public readonly string cogs_R = masterConfigPath + @"cogs\";
+        static public readonly string reversers_R = masterConfigPath + @"ukws\";
+        static public readonly string messages_R = masterConfigPath + @"messages\";
+        static public readonly string switchboard_R = masterConfigPath + @"switchboard\";
+
+
+        }
+}
