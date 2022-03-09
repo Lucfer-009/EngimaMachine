49ffe58e0742bb42f7326f73c22940099cf2b6d6
Brad <bradspam.bw@gmail.com>
2022-03-09 07:50:22 +0000

    Minor changes

--- Brad's Engima Machine/Program.cs
+++ Brad's Engima Machine/Program.cs
@@ -15,20 +15,20 @@ namespace Brad_s_enigma_Machine
         
     }
     class Instance
-    {
+    { // Main overarching class that handles the loading of both the Machine class and the Fitness class.
         private static int next_ID = 0;
         public int ID;
 
         public Instance()
         {
-            ID = next_ID;
+            ID = next_ID; // Loads the ID
             next_ID++;
         }
 
         public void Start()
         {
             bool end = false;
-            while (end == false)
+            while (end == false) // Ensures this will loop if an invalid entry is given.
             {
                 GU.Print("1. Enigma Machine");
                 GU.Print("2. Enigma Decryption Program");
@@ -48,22 +48,22 @@ namespace Brad_s_enigma_Machine
                 }
                 else if (choice == '3')
                 {
-                    end = true;
+                    end = true; // Ends the program
                 }
                 else
                 {
-                    GU.Print("ERROR | Enter a valid choice, 1-3");
+                    GU.Print("ERROR | Enter a valid choice, 1-3"); // Entry error, anything with "ERROR |" means it will loop back to a user input
                 }
             }
         }
 
         private void LoadCypherBreaking()
-        {
+        { // The current class for handling fitness attacks on text (in early development)
             Fitness currentBreaker;
             int loadingChoice = -1;
             int speedChoice = -1;
             bool valid = false;
-            while (valid == false)
+            while (valid == false) // Ensures an invalid response loops back to user input.
             {
                 GU.Print("-- -- ");
                 GU.Print("1. Load from file");
@@ -74,7 +74,7 @@ namespace Brad_s_enigma_Machine
                 GU.Print("-- -- ");
 
                 loadingChoice = GU.GetIntFromUser("Enter choice [1-2]");
-                if (loadingChoice == 1 || loadingChoice == 2)
+                if (loadingChoice == 1 || loadingChoice == 2) // catch clause for invalid responses.
                 {
                     valid = true;
                 }
@@ -86,11 +86,11 @@ namespace Brad_s_enigma_Machine
             }
             GU.Print("");
             valid = false;
-            while (valid == false)
+            while (valid == false) // regardless of file loading or string entry the user must select the focus
             {
                 GU.Print("-- -- ");
-                GU.Print("1. Accuracy Focus");
-                GU.Print("2. Speed Focus");
+                GU.Print("1. Accuracy Focus"); // larger dataset, 5-gram = 4000 entries
+                GU.Print("2. Speed Focus"); // much smaller dataset, 5-gram = 512 entries
                 GU.Print("");
                 GU.Print("-- -- ");
                 speedChoice = GU.GetIntFromUser("Enter choice [1-2]");
@@ -105,17 +105,17 @@ namespace Brad_s_enigma_Machine
                 }
             }
             string writtenText;
-            if (loadingChoice == 1)
+            if (loadingChoice == 1) // Near identical code to File Read.
             {
                 writtenText = "";
                 string locationOfText = "";
                 bool acceptable = false;
                 while (acceptable == false)
                 {
-                    GU.PrintContentsOfDirectory(FileLocationHandler.cyphertextMessages_R);
+                    GU.PrintContentsOfDirectory(FileLocationHandler.cyphertextMessages_R); // Prints the contents of the messages folder
                     locationOfText = GU.GetStringFromUser("Please enter the file name of message (do NOT indlude the .txt)\n (IF NOT IN \\cracking\\messages enter !)");
 
-                    if (locationOfText == "!")
+                    if (locationOfText == "!") // gives the user the option to provide a full address
                     {
                         locationOfText = GU.GetStringFromUser("Enter the full address");
                     }
@@ -124,7 +124,7 @@ namespace Brad_s_enigma_Machine
                         locationOfText = $"{FileLocationHandler.cyphertextMessages_R}{locationOfText}.txt";
                     }
 
-                    if (File.Exists(locationOfText) == true)
+                    if (File.Exists(locationOfText) == true) // Ensures the file exsists.
                     {
                         acceptable = true;
                     }
